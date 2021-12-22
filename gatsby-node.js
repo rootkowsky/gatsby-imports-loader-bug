11b29b9fb240e334603e9722741ff750708dee03
Krzysztof Pogwizd <pogwizdkrzysztof@gmail.com>
2021-12-22 16:06:47 +0100

    Add imports-loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,31 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+const path = require('path');
+
+exports.onCreateWebpackConfig = (args) => {
+  const { actions } = args;
+  const { setWebpackConfig } = actions;
+
+  setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: path.resolve(__dirname, './gatsby-browser.js'),
+          use: [
+            {
+              loader: 'imports-loader',
+              options: {
+                type: 'module',
+                imports: [
+                  {
+                    syntax: 'side-effects',
+                    moduleName: path.resolve(__dirname, './assets/extras.css'),
+                  }
+                ],
+              },
+            },
+          ]
+        }
+      ]
+    }
+  });
+
 }
